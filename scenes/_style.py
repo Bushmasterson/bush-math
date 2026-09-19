5c5eaf199a4f80e653353c2208846df3d516a568
bushmasterson <bushmasterson@proton.me>
2026-09-19 10:50:40 +0300

    feat: bush-math palette and styled scene
    - Add _style.py with black + yellow + blue palette
    - Extend Scene as StyledScene with manim.cfg background
    - Refactor unit_circle.py to use MAIN_YELLOW / MAIN_BLUE

---
+++ scenes/_style.py
@@ -0,0 +1,12 @@
+# pyright: reportWildcardImportFromLibrary=false
+
+from manim import *
+from manim.utils.color import ManimColor
+
+BG_COLOR = ManimColor("#000000")
+MAIN_YELLOW = ManimColor("#F7D96F")
+MAIN_BLUE = ManimColor("#58C4DD")
+
+
+class StyledScene(Scene):
+    pass
